e857b7260e7e581fa4b5a9bb5fb577370770d1f2
snecfest <snecfest@gmail.com>
2025-01-18 04:57:49 +0000

    ok

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -21,13 +21,12 @@ const Table = ({ data, onDelete, refetchPrograms }) => {
 
     // Prevent duplicates for non-General categories
     if (
-      ((uniqueCode === 317 || uniqueCode === 212) && existingStudents.length >= 0) ||
-      (categoryName !== "General" && existingStudents.length >= 1)
+      categoryName !== "General" &&
+      (uniqueCode !== 317 && existingStudents.length > 1 || uniqueCode !== 212 && existingStudents.length > 1)
     ) {
-      toast.error("This category only allows the specified number of students.");
+      toast.error("This category only allows one student.");
       return;
     }
-    
     
 
     try {
