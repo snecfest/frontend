c55743b6abdf5a7163cba4293ec084c2a16a5ddf
snecfest <snecfest@gmail.com>
2025-01-20 07:19:39 +0000

    base multy add

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,13 +1,16 @@
 import axios from "axios";
 import Cookies from "js-cookie";
-export const axiosInstance=axios.create({
-    baseURL:'https://resgister-theta.vercel.app/api',
-    headers: {
-        "Content-Type": "application/json",
-      },
-      withCredentials: true,
+const baseURL = window.location.hostname === 'snec.vercel.app'
+  ? 'https://resgister-theta.vercel.app/api'
+  : 'https://she-backend.vercel.app/api';
 
-})
+export const axiosInstance = axios.create({
+  baseURL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+  withCredentials: true,
+});
 
 axiosInstance.interceptors.request.use(
   (config) => {
